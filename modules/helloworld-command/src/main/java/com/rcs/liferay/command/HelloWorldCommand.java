57c8fe4fe1203084621c52e83c0ae62c633a81bd
jrakhman <juniar.rahman@rithm.nl>
2019-11-04 14:41:26 +0700

    added uppercase service

--- modules/helloworld-command/src/main/java/com/rcs/liferay/command/HelloWorldCommand.java
+++ modules/helloworld-command/src/main/java/com/rcs/liferay/command/HelloWorldCommand.java
@@ -3,6 +3,9 @@ package com.rcs.liferay.command;
 import com.rcs.liferay.hello.api.HelloService;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
 
 /**
  * @author jrakhman
@@ -17,11 +20,26 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class HelloWorldCommand {
 
-	@Reference
+
 	private HelloService helloService;
 
 
 	public void say(String what) {
 		System.out.println(helloService.sayHello(what));
+	}
+
+	@Reference(policy = ReferencePolicy.DYNAMIC,
+			policyOption = ReferencePolicyOption.GREEDY,
+			cardinality = ReferenceCardinality.MANDATORY)
+	protected void setHelloService(HelloService helloService) {
+		System.out.println("Set hello service : " + helloService.getClass().getName());
+		this.helloService = helloService;
+	}
+
+	protected void unsetHelloService(HelloService helloService) {
+		System.out.println("Unset hello service : " + helloService.getClass().getName());
+		if (helloService == this.helloService) {
+			this.helloService = null;
+		}
 	}
 }
--- modules/helloworld-uppercase-service/src/main/java/com/rcs/liferay/hello/uppercase/impl/HelloUppercaseServiceImpl.java
+++ modules/helloworld-uppercase-service/src/main/java/com/rcs/liferay/hello/uppercase/impl/HelloUppercaseServiceImpl.java
@@ -1,6 +1,8 @@
-package com.rcs.liferay.hello.impl;
+package com.rcs.liferay.hello.uppercase.impl;
 
+import com.liferay.portal.kernel.util.StringUtil;
 import com.rcs.liferay.hello.api.HelloService;
+
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -9,12 +11,13 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
+		"service.ranking:Integer=100"
 	},
 	service = HelloService.class
 )
-public class HelloServiceImpl implements HelloService {
+public class HelloUppercaseServiceImpl implements HelloService {
 	@Override
 	public String sayHello(String what) {
-		return "Hello " + what + " " + what;
+		return StringUtil.upperCase(what);
 	}
 }
