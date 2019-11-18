730738f789d4360d43199fc16b13e012bf3bedc8
jrakhman <juniar.rahman@rithm.nl>
2019-11-18 12:20:42 +0700

    added training portlet

--- modules/helloworld-service/src/main/java/com/rcs/liferay/hello/impl/HelloServiceImpl.java
+++ modules/helloworld-service/src/main/java/com/rcs/liferay/hello/impl/HelloServiceImpl.java
@@ -7,14 +7,15 @@ import org.osgi.service.component.annotations.Component;
  * @author jrakhman
  */
 @Component(
-	immediate = true,
-	property = {
-	},
+		immediate = true,
+		property = {
+				"service.ranking:Integer=99"
+		},
 	service = HelloService.class
 )
 public class HelloServiceImpl implements HelloService {
 	@Override
 	public String sayHello(String what) {
-		return "Hello " + what + " " + what;
+		return "Hello " + what + " " + what + " " + what;
 	}
 }
